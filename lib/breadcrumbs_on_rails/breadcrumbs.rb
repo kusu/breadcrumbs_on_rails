850af46ab3622a978649655e818dcda110b848f3
hirooooooo <hiroaki.suzuki@codecamp.jp>
2016-11-30 20:26:10 +0900

    bugfix

--- lib/breadcrumbs_on_rails/breadcrumbs.rb
+++ lib/breadcrumbs_on_rails/breadcrumbs.rb
@@ -89,7 +89,8 @@ module BreadcrumbsOnRails
           content = compute_name(element)
         else
           span = @context.content_tag("span", compute_name(element), itemprop: :title)
-          content = @context.link_to_unless_current(span, compute_path(element), element.options.merge({itemscope: true, itemtype: "http://data-vocabulary.org/Breadcrumb", itemprop: :url}))
+          content = @context.link_to_unless_current(span, compute_path(element), element.options.merge({itemprop: :url}))
+          content = @context.content_tag("span", content, {itemscope: true, itemtype: "http://data-vocabulary.org/Breadcrumb"})
         end
         if @options[:tag]
           @context.content_tag(@options[:tag], content)
